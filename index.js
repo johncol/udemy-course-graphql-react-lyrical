06555cf45d5fe5e9a0515988a4a72d708f51a231
John <john.19col@gmail.com>
2019-12-26 18:24:49 -0500

    Log in terminal URLs

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = require('./server/server');
 
 app.listen(4000, () => {
-  console.log('Listening');
+  console.log(`
+  ------------------------------------------------------------------------------------------
+    Server started on http://localhost:4000
+    GraphiQL on http://localhost:4000/graphql
+  ------------------------------------------------------------------------------------------
+  `);
 });
