551f2d1a5762859e1989d55d699c82611ef5de20
John <john.19col@gmail.com>
2019-12-29 15:29:52 -0500

    Connect SongList component to GraphQL

--- client/components/SongsList.js
+++ client/components/SongsList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export const SongsList = ({}) => {
-  const songs = [
-    { id: '1', title: 'Perdona' },
-    { id: '2', title: 'Perfect' },
-    { id: '3', title: 'Goodbyt My Lover' }
-  ];
+const SongsList = ({ data: { loading, songs } }) => {
+  if (loading) {
+    return <div>loading..</div>;
+  }
+
   return (
     <ul className="collection">
       {songs.map(song => (
@@ -16,3 +17,16 @@ export const SongsList = ({}) => {
     </ul>
   );
 };
+
+const query = gql`
+  query {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+const SongsListGraphQL = graphql(query)(SongsList);
+
+export { SongsListGraphQL as SongsList };
