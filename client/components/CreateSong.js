eb4519fc8a14b7a16734483b5c5bebfc77016896
John <john.19col@gmail.com>
2019-12-30 14:20:52 -0500

    Connect CreateSong component with graphql mutation

--- client/components/CreateSong.js
+++ client/components/CreateSong.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
 
-export const CreateSong = ({}) => {
+import { Mutation, Query } from './../graphql';
+
+export const CreateSong = props => {
   const [title, setTitle] = useState('');
   const updateTitle = event => setTitle(event.target.value);
+
+  const history = useHistory();
+  const navigateToList = () => history.push('/songs');
+
+  const [createSong] = useMutation(Mutation.createSong, {
+    ignoreResults: true,
+    onCompleted: navigateToList,
+    refetchQueries: [
+      {
+        query: Query.fetchSongs
+      }
+    ]
+  });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    createSong({ variables: { title } });
+  };
 
   return (
     <React.Fragment>
       <h4>Add a new Song</h4>
-      <form autoComplete="off">
-        <label htmlFor="title"></label>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
         <input id="title" placeholder="e.g. Crazy" value={title} onChange={updateTitle} />
       </form>
     </React.Fragment>
