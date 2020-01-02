d04785b335beb0185cdbd5dc70748e32de2ca4a4
John <john.19col@gmail.com>
2020-01-02 15:31:35 -0500

    Create mutation to add a lyric to a song

--- client/graphql/mutations/index.js
+++ client/graphql/mutations/index.js
@@ -1,7 +1,9 @@
 import { createSong } from './create-song';
 import { deleteSong } from './delete-song';
+import { addLyricToSong } from './add-lyric-to-song';
 
 export const Mutation = {
   createSong,
-  deleteSong
+  deleteSong,
+  addLyricToSong
 };
