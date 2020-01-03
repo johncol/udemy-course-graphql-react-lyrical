4088196b0dbaea9a75045629e8b520114a591ba2
John <john.19col@gmail.com>
2020-01-03 04:34:31 -0500

    Show thump up icon for each lyric item

--- client/components/LyricsList.js
+++ client/components/LyricsList.js
@@ -4,10 +4,19 @@ export const LyricsList = ({ lyrics }) => {
   return (
     <ul className="collection">
       {lyrics.map(lyric => (
-        <li className="collection-item" key={lyric.id}>
-          {lyric.content}
-        </li>
+        <LyricItem key={lyric.id} lyric={lyric} />
       ))}
     </ul>
+  );
+};
+
+const LyricItem = ({ lyric }) => {
+  return (
+    <li className="collection-item" style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <span>{lyric.content}</span>
+      <i className="material-icons" style={{ cursor: 'pointer' }} onClick={() => console.log('id', lyric.id)}>
+        thumb_up
+      </i>
+    </li>
   );
 };
