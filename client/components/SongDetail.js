b5405697114e15c063f9cefa99ba9a6416fcbdab
John <john.19col@gmail.com>
2020-01-03 04:46:27 -0500

    Remove unsed Link import

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 
 import { Query } from './../graphql';
