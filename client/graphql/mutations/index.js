c9618b3db72eb4d1ea9257080e8935ec36da3426
John <john.19col@gmail.com>
2020-01-04 17:05:11 -0500

    Add likeLyric mutation

--- client/graphql/mutations/index.js
+++ client/graphql/mutations/index.js
@@ -1,9 +1,11 @@
 import { createSong } from './create-song';
 import { deleteSong } from './delete-song';
 import { addLyricToSong } from './add-lyric-to-song';
+import { likeLyric } from './like-lyric';
 
 export const Mutation = {
   createSong,
   deleteSong,
-  addLyricToSong
+  addLyricToSong,
+  likeLyric
 };
---
+++ client/graphql/mutations/like-lyric.js
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag';
+
+export const likeLyric = gql`
+  mutation LikeLyric($id: ID!) {
+    likeLyric(id: $id) {
+      likes
+    }
+  }
+`;
